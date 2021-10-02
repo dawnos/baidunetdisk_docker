4c9528a42c9a629031a8653008d75336b87f6c6a
dawnos <litang.cv@gmail.com>
2021-10-03 00:48:14 +0800

    update to fit new noVNC

--- baidunetdisk_entrypoint.sh
+++ baidunetdisk_entrypoint.sh
@@ -5,7 +5,7 @@ chmod a+rwx /root/baidunetdisk
 chmod a+rwx /root/baidunetdiskdownload
 
 /usr/bin/vncserver -geometry 1600x1200 2>&1
-/opt/noVNC/utils/launch.sh --vnc localhost:5901 2>&1 &
+/opt/noVNC/utils/novnc_proxy --vnc localhost:5901 2>&1 &
 echo "Access through VNC (5091) or browser (localhost:6080) using password: 12345678."
 DISPLAY=:1 /opt/baidunetdisk/baidunetdisk 2>&1
 
